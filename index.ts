6f6d8d5b906218a3b6b99456e8cfd561e8ec4555
Hardik <hardikganatra20@gmail.com>
2021-06-05 19:49:56 +0530

    TypeScript Started

---
+++ index.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const url = "https://jsonplaceholder.typicode.com/todos/1"
+
+interface Todo  {
+  id: number
+}
+axios.get(url).then((res) => {
+  const todo = res.data  as Todo;
+  const ID = todo.id
+  console.log(ID)
+})
